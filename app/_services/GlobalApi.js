3bb78f6d3c03090abd03f3348f91868c90bfb87d
Laibah-Shahid <laibahshahid18@gmail.com>
2024-08-16 23:13:27 +0500

    initial commit

--- app/_services/GlobalApi.js
+++ app/_services/GlobalApi.js
@@ -22,6 +22,33 @@ const getCategory=async()=>{
       const result=await request(MASTER_URL,query)
     return result
 }
+
+const getAllBusinessList=async()=>{
+    const query=gql`
+    query BusinessList {
+  businessLists {
+    about
+    address
+    category {
+      name
+    }
+    contactPerson
+    email
+    images {
+      url
+    }
+    id
+    name
+  }
+}
+`
+
+
+const result=await request(MASTER_URL,query)
+return result
+}
+
 export default{
-    getCategory
+    getCategory,
+    getAllBusinessList
 }
--- app/page.js
+++ app/page.js
@@ -4,25 +4,40 @@ import Hero from "./_components/Hero";
 import CategoryList from "./_components/CategoryList";
 import GlobalApi from "./_services/GlobalApi";
 import { useEffect, useState } from "react";
+import BusinessList from "./_components/BusinessList";
 
 
 export default function Home() {
 
   const [categoryList,setCategoryList]=useState([]);
+  const [businessList,setBusinessList]=useState([]);
   useEffect(()=>{
     getCategoryList();
+    getAllBusinessList();
   },[])
 
+// use to get all category list
   const getCategoryList=()=>{
     GlobalApi.getCategory().then(resp=>{
       setCategoryList(resp.categories);
     })
   }
+
+  // use to get all business list
+const getAllBusinessList=()=>{
+    GlobalApi.getAllBusinessList().then(resp=>{
+      setBusinessList(resp.businessLists)
+    })
+}
+
   return (
     <div>
       <Hero/>
 
       <CategoryList categoryList={categoryList}/>
+
+      <BusinessList businessList={businessList}
+      title={"Popular Business"}/>
     </div>
   );
 }
